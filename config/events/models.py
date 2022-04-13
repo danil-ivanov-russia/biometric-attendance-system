e8579324d9da04e6d68c4fd1dadf5a09ba0c9111
Danil Ivanov <danil.ivanov.russia@gmail.com>
2022-04-13 17:49:25 +0300

    Created Attendee and Biometrics models

--- config/events/models.py
+++ config/events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class Attendee(AbstractUser):
+    patronymic = models.CharField(max_length=150, blank=True)
+
+    def get_full_name(self):
+        full_name = "%s %s %s" % (self.first_name, self.last_name, self.patronymic)
+        return full_name.strip()
+
+
+class Biometrics(models.Model):
+    owner = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+    facial_data = models.BinaryField()
