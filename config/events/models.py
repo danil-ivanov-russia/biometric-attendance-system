aec832198223fe494efcd46e6b09b2bc16b01f52
Danil Ivanov <danil.ivanov.russia@gmail.com>
2022-04-16 10:47:08 +0300

    Added basic event creation

--- config/events/models.py
+++ config/events/models.py
@@ -1,26 +1,32 @@
 import datetime
+import uuid
 
 from django.db import models
+from django.utils import timezone
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class Attendee(AbstractUser):
-    patronymic = models.CharField(max_length=150, blank=True)
-
-    def get_full_name(self):
-        full_name = "%s %s %s" % (self.first_name, self.last_name, self.patronymic)
-        return full_name.strip()
+# class Attendee(AbstractUser):
+#     patronymic = models.CharField(max_length=150, blank=True)
+#
+#     def get_full_name(self):
+#         full_name = "%s %s %s" % (self.first_name, self.last_name, self.patronymic)
+#         return full_name.strip()
 
 
 class Biometrics(models.Model):
-    owner = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+    # owner = models.ForeignKey(Attendee, on_delete=models.CASCADE)
     facial_data = models.BinaryField()
 
 
 class Event(models.Model):
     name = models.CharField(max_length=200, blank=True)
-    date = models.DateTimeField()
-    attendees = models.ManyToManyField(Attendee)
+    uuid = models.CharField(max_length=36, blank=True)
+    datetime = models.DateTimeField()
+    # attendees = models.ManyToManyField(Attendee)
+
+    def __str__(self):
+        return self.name
 
--- config/events/views.py
+++ config/events/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
+from .forms import EventForm
+from .models import Event
+import uuid
+
 
 # Create your views here.
+# class CreateEventView(generic.FormView):
+def create_event(request):
+    form = EventForm()
+
+    if request.method == 'POST':
+        #print(request.POST)
+        event = Event(uuid=str(uuid.uuid4()), datetime=timezone.now())
+        print(event.uuid)
+        form = EventForm(request.POST, instance=event)
+        #form = EventForm(request.POST, initial={'uuid': str(uuid.uuid4())})
+        if form.is_valid():
+            #print(form)
+            form.save()
+
+    context = {'form':form}
+    template_name = 'events/create.html'
+
+    return render(request, template_name, context)
+
